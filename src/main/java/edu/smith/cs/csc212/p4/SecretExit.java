ba529bcba36c9a2178ae76f595e44db23f903908
Valeria <Valeria@131.229.108.111>
2019-02-21 21:35:27 -0500

    more comments to finished code

--- src/main/java/edu/smith/cs/csc212/p4/SecretExit.java
+++ src/main/java/edu/smith/cs/csc212/p4/SecretExit.java
@@ -6,13 +6,25 @@ public class SecretExit extends Exit {
 	 */
 	private boolean hidden = true; 
 	
+	/**
+	 * creates constructor for SecretExit
+	 * @param target
+	 * @param description
+	 */
 	public SecretExit(String target, String description) {
 		super(target, description);
 	}
+	/**
+	 * (non-Javadoc) true/false if the exit is a secret
+	 * @see edu.smith.cs.csc212.p4.Exit#isSecret()
+	 */
 	public boolean isSecret() {
 		return hidden;
 	}
-	
+	/**when the user enters search hidden is false so user can see exit
+	 * (non-Javadoc)
+	 * @see edu.smith.cs.csc212.p4.Exit#search()
+	 */
 	public void search() {
 		hidden = false;
 			
